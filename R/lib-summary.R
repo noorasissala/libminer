a3c7d8ce17b37a5bac75d4bdbcc024b4a06bae88
Noora Sissala <noora.sissala@ki.se>
2024-06-14 11:22:20 +0200

    Added lib_summary.R, documentation, and license.

---
+++ R/lib-summary.R
@@ -0,0 +1,46 @@
+#' R Library Summary
+#'
+#' Provides a brief summary of the package libraries on
+#' your machine.
+#'
+#' @param sizes A logical value indicating whether to calculate sizes.
+#' Default `FALSE`.
+#'
+#' @return A `data.frame` containing the count of packages in each
+#'   of the user's libraries. If `sizes = TRUE`, a column of library
+#'   sizes will be added.
+#' @export
+#'
+#' @examples
+#' lib_summary()
+#' lib_summary(sizes = TRUE)
+lib_summary <- function(sizes = FALSE) {
+  if (!is.logical(sizes)) {
+    stop("sizes must be a logical value (TRUE/FALSE)")
+  }
+
+  pkgs <- utils::installed.packages()
+  pkg_tbl <- table(pkgs[, "LibPath"])
+  pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
+  names(pkg_df) <- c("Library", "n_packages")
+
+  if (isTRUE(sizes)) {
+    pkg_df <- calculate_sizes(pkg_df)
+  }
+
+  pkg_df
+}
+
+#' calculate sizes
+#'
+#' @param df a data frame of Libraries
+#' @noRd
+#'
+#' @return a data frame with a lib_size column
+calculate_sizes <- function(df) {
+  df$lib_size <- map_dbl(
+    df$Library,
+    ~ sum(fs::file_size(fs::dir_ls(.x, recurse = TRUE)))
+  )
+  df
+}
